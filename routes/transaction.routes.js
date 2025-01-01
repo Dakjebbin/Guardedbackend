190b62dca8a98a464d9551dcd9efa439d322300e
DakJebbin <Jebbinp@gmail.com>
2025-01-01 22:16:50 +0100

    admin check

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { validateUser } from "../middlewares/validateUser.js";
 import { getTransaction, transact, updateTransactionStatus } from "../controllers/transaction.controllers.js";
+import { adminCheck } from "../middlewares/user.status.js";
 
 const router = express.Router();
 
 router.post("/userTransact", validateUser, transact)
 router.get("/getTransact/:username", validateUser, getTransaction)
-router.patch("/updateTransact/:id", validateUser, updateTransactionStatus)
+router.patch("/updateTransact/:id", validateUser, adminCheck, updateTransactionStatus)
 export default router;
